dcf476eab920fa002c8e1577464154acdf5addd7
jhnferraris <contact@jhnferraris.dev>
2022-05-19 08:34:09 +0800

    Initial commit working sample

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json())
+
+app.post('/webhook', async (req, res) => {
+  // TODO: You will have to implement the logic of this method.
+  processWebhookNotificationData(req.body);
+  res.send({
+    success: true,
+  })
+});
+
+app.listen(8080, () => console.log(`Listening on port ${8080}!`));
