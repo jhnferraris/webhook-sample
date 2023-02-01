80a89de293923482fface42074369f9639fd01c8
jhnferraris <contact@jhnferraris.dev>
2023-02-01 12:54:20 +0800

    Added logs

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ app.use(bodyParser.json())
 
 app.post('/webhook', async (req, res) => {
   // TODO: You will have to implement the logic of this method.
-  // processWebhookNotificationData(req.body);
+  console.log(req.url);
+  processWebhookNotificationData(req.body);
   res.send({
     processWebhookData: 'success',
   })
 });
+
+const processWebhookNotificationData = (data) => {
+  console.log(`Processing data`);
+  console.log(data);
+  console.log(`Done`)
+}
 
 app.listen(8080, () => console.log(`Listening on port ${8080}!`));
